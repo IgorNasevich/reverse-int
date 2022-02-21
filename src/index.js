cd442f16cf8ed4dc0f084183c65b98b89eee0420
IgorNasevich <igor.nasevich007@gmail.com>
2022-02-21 21:32:05 +0300

    fix: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        n *= -1;
+    }
+
+    let result = 0;
+
+    while(n > 0){
+        result *= 10;
+        result += n % 10;
+        n = Math.trunc(n / 10);
+    }
+    return result;
 }
